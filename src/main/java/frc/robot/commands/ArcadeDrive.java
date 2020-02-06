47e12dd11097865eb2235d70e500e2009acc7755
JVViacrusis <45835322+JVViacrusis@users.noreply.github.com>
2020-02-06 10:21:34 -0800

    Refresh, Init, Drivetrain
    Refresh:
    	- Deleted all repo testing files
    
    Init:
    	- First real upload to repo
    
    Drivetrain:
    	- Basic drivetrain code.
    		- needs tuning of controller axes.

---
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -0,0 +1,65 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Drivetrain;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+
+
+public class ArcadeDrive extends CommandBase 
+{
+  /**
+   * Creates a new ArcadeDrive.
+   */
+
+
+  private final Drivetrain __Drivetrain;
+  private final Joystick __Joystick;
+
+  public ArcadeDrive() 
+  {
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    __Drivetrain = RobotContainer.m_DriveTrain;
+    __Joystick = RobotContainer.m_joystick;
+
+    addRequirements(__Drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() 
+  {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() 
+  {
+    __Drivetrain.DT_ArcadeDrive(__Joystick.getRawAxis(0), __Joystick.getRawAxis(1));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) 
+  {
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() 
+  {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,68 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import frc.robot.Constants;
+
+
+
+
+public class Drivetrain extends SubsystemBase 
+{
+  /**
+   * Creates a new Drivetrain.
+   */
+
+  private final WPI_VictorSPX l_b_Speedcontroller;
+  private final WPI_VictorSPX l_f_SpeedController;
+  private final SpeedControllerGroup l_ControllerGroup;
+  
+  private final WPI_VictorSPX r_b_SpeedController;
+  private final WPI_VictorSPX r_f_SpeedController;
+  private final SpeedControllerGroup r_ControllerGroup;
+
+  private final DifferentialDrive m_Drive;
+
+  public Drivetrain() 
+  {
+    l_b_Speedcontroller = new WPI_VictorSPX(Constants.DT_MOTOR_L_B_Port);
+    l_f_SpeedController = new WPI_VictorSPX(Constants.DT_MOTOR_L_F_Port);
+    l_ControllerGroup = new SpeedControllerGroup(l_b_Speedcontroller, l_f_SpeedController);
+
+
+    r_b_SpeedController = new WPI_VictorSPX(Constants.DT_MOTOR_R_B_Port);
+    r_f_SpeedController = new WPI_VictorSPX(Constants.DT_MOTOR_R_F_Port);
+    r_ControllerGroup = new SpeedControllerGroup(r_b_SpeedController, r_f_SpeedController);
+
+    m_Drive = new DifferentialDrive(l_ControllerGroup, r_ControllerGroup);
+  }
+
+
+  public void DT_ArcadeDrive(double i_Speed, double i_Rotation)
+  {
+    m_Drive.arcadeDrive(i_Speed * Constants.DT_SPEED_MULTIPLIER,
+                        i_Rotation * Constants.DT_ROTATION_MULTIPLIER);
+  }
+
+  public void DT_Stop()
+  {
+    m_Drive.arcadeDrive(0, 0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+  }
+}
